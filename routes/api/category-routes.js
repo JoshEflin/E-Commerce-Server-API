b6fdabed5bfbe81bb11b4b46c5f4beb94d4a19ba
ragingpenguin <eflinjh@gmail.com>
2023-02-28 14:31:20 -0500

    -m finished categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,31 +4,66 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
+  // find all categories
   const categories = await Category.findAll({
+    //include its associated Products
     include: [{model: Product}]
   }).catch((err)=>{
     res.status(500).json(err)
   });
   res.status(200).json(categories)
-  // find all categories
-  // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value params.id holds the value of the request ID
+  try {const category = await Category.findByPk(req.params.id,{
+    // be sure to include its associated Products
+    include:[{model: Product}]
+    });
+    res.status(200).json(category)
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+  const newCat = await Category.create(req.body, {
+    
+    include:[{model: Product}]
+    });
+    res.status(200).json(newCat)
+  }catch(err){
+    res.status(500).json(err);
+  
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updateCat = await Category.update(req.body, {
+      // update a category by its `id` value
+      where: {id :req.params.id},
+      include:[{model: Product}]
+      });
+      res.status(200).json(updateCat)
+    }catch(err){
+      res.status(500).json(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const updateCat = await Category.destroy({
+      // delete a category by its `id` value
+      where: {id :req.params.id},
+      include:[{model: Product}]
+      });
+      res.status(200).json(updateCat)
+    }catch(err){
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
